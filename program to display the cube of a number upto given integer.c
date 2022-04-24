bc3399377ccbff346c1b8653355c5b043c434265
Leenjerono2001 <102831045+Leenjerono2001@users.noreply.github.com>
2022-04-24 17:05:37 +0300

    Add files via upload

---
+++ program to display the cube of a number upto given integer.c
@@ -0,0 +1,13 @@
+// program to display cube of a number upto given integer
+#include <stdio.h>
+int main()
+ {
+    int i,ctr;
+    printf("Input number of terms : ");
+    scanf("%d", &ctr);
+    for(i=1;i<=ctr;i++)
+    {
+	 printf("Number is : %d and cube of the %d is :%d \n",i,i, (i*i*i));     
+    }
+  return 0;
+ }
